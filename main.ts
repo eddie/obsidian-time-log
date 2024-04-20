69275cf7946c8b9576b657cb25af11dcee9ff56e
Eddie Blundell <eddie@eddieblundell.co.uk>
2024-04-20 17:50:28 +0100

    feat: handle lists and some initial settings

--- main.ts
+++ main.ts
@@ -1,19 +1,30 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, debounce } from 'obsidian';
+import moment from 'moment';
+import { App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting, debounce } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+// TODO: Add replacementInterval to settings modal.
+// TODO: Add format option for log lines
+// TODO: Setting to force list or not.
+// TODO: Document property toggle for enable
+// TODO: Audo header insertion with todays note linked
+// TODO: Setting for formatting log titles and detecting
+
+interface TimelogSettings {
 	mySetting: string;
-	replacementInterval: number
+	replacementInterval: number;
+	useList: boolean;
+	dateFormat: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: TimelogSettings = {
 	mySetting: 'default',
-	replacementInterval: 5
+	replacementInterval: 5,
+	useList: false,
+	dateFormat: 'HH:MM'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TimelogPlugin extends Plugin {
+	settings: TimelogSettings;
 
 	lastReplacement?: Date;
 
@@ -22,7 +33,7 @@ export default class MyPlugin extends Plugin {
 
 		// Watch for changes to intercept log lines
 		this.registerEvent(this.app.workspace.on('editor-change',
-			debounce(this.onEditorChange.bind(this), 1000, true)
+			debounce(this.onEditorChange.bind(this), 1000, false)
 		));
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
@@ -33,14 +44,14 @@ export default class MyPlugin extends Plugin {
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Start Log Entry',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
+			editorCallback: (editor: Editor) => {
 				console.log(editor.getSelection());
 				editor.replaceSelection('## Log {date}\n\n');
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new TimelogSettingTab(this.app, this));
 	}
 
 	onEditorChange(editor: Editor, view: MarkdownView) {
@@ -55,16 +66,32 @@ export default class MyPlugin extends Plugin {
 	insertLogLine(editor: Editor) {
 		// Insert a log line below the current line
 		const cursor = editor.getCursor();
-		const logDate = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-		const logHeader = `- **${logDate}**: `;
+		let logDate;
+		try {
+			logDate = moment().format(this.settings.dateFormat);
+		} catch (e) {
+			logDate = moment().format(DEFAULT_SETTINGS.dateFormat)
+		}
 
-		editor.replaceRange(logHeader, { line: cursor.line, ch: 0 });
+		const logHeader = `**${logDate}**: `;
+		const spacing = 2;
+		let offset = editor.getLine(cursor.line).indexOf('-');
+
+		// Account for case where hyphen is found, even at position 0
+		if (offset >= 0) {
+			offset = offset + spacing;
+		} else {
+			offset = 0;
+		}
+		editor.replaceRange(logHeader, { line: cursor.line, ch: offset });
 		editor.setCursor({ line: cursor.line, ch: cursor.ch + logHeader.length });
 	}
 
 	isInsideLogConext(editor: Editor) {
+		const interval = 5 // this.settings.replacementInterval;
 		// Determine if last replacement run in past X minutes
-		if (this.lastReplacement && (new Date().getTime() - this.lastReplacement.getTime()) < this.settings.replacementInterval * 1000) {
+		if (this.lastReplacement &&
+			(new Date().getTime() - this.lastReplacement.getTime()) < interval * 1000) {
 			return false;
 		}
 
@@ -74,7 +101,7 @@ export default class MyPlugin extends Plugin {
 		let line = editor.getLine(currentLine);
 
 		// Don't double log.
-		if (this.isLogHeader(line)) {
+		if (this.isLoggedLine(line)) {
 			return false;
 		}
 
@@ -99,15 +126,18 @@ export default class MyPlugin extends Plugin {
 		return line.startsWith('#');
 	}
 
-	isOnLoggedLine(line: string) {
-		// Does line start with - **HH:MM**:? 
-		const timeRegex = /- \*\*\d{1,2}:\d{2}\*\*: /;
-		return timeRegex.test(line);
+	isLoggedLine(line: string) {
+		// Remove all optional spaces and hyphens from start of line
+		const l = line.replace(/^[-\s]/g, '')
+			// Remove formatting
+			.replace(/\*/g, '')
+			// Now take the substr of the length of the time format
+			.substring(0, this.settings.dateFormat.length);
+
+		return moment(l, this.settings.dateFormat).isValid();
 	}
 
 	onunload() {
-
-
 
 	}
 
@@ -120,26 +150,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class TimelogSettingTab extends PluginSettingTab {
+	plugin: TimelogPlugin;
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TimelogPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -150,14 +164,27 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setHeading()
+			.setName('Timelog Settings');
+
+		new Setting(containerEl)
+			.setName('Minimum Log Duration')
+			.setDesc('Minimum time in minutes between log entries being prefixed')
+			.addSlider(slider => slider.setValue(this.plugin.settings.replacementInterval ? this.plugin.settings.replacementInterval / 60 : 1)
+				.setLimits(1, 60, 1)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.replacementInterval = value * 60;
 					await this.plugin.saveSettings();
 				}));
+
+
+		new Setting(containerEl)
+			.setName('Log Format')
+			.setDesc('Format of timestamp to prefix log entries e.g HH:MM')
+			.addMomentFormat((format) => format.setValue(this.plugin.settings.dateFormat ?? DEFAULT_SETTINGS.dateFormat)
+				.onChange(async (value) => {
+					this.plugin.settings.dateFormat = value;
+					await this.plugin.saveSettings();
+				}))
 	}
 }
